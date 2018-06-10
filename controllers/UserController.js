97abd1c58bf13454fb3e8e77794868e6c11f92d2
Matheus de Araujo Delatorre <matheus.a.delatorre@gmail.com>
2018-06-10 19:18:39 -0300

    Adicionando arquivo enviado pelo usuário a página html utilizando FileReader()

---
+++ controllers/UserController.js
@@ -0,0 +1,109 @@
+class UserController{
+    
+    constructor(formId, tableId){
+        this.formEl = document.getElementById(formId);
+        this.tableEl = document.getElementById(tableId);
+        this._onSubmit();
+    }
+
+    _onSubmit(){
+        this.formEl.addEventListener(("submit"), event => {
+            event.preventDefault();
+
+            let values = this._getValues();
+
+            this._getPhoto((content) => {
+                values.photo = content;
+                this._addLine(values); // Adiciona linha ao html
+            });
+
+        });
+    }
+
+    _getPhoto(callBack){
+        let fileReader = new FileReader();
+
+        let filePhoto = [...this.formEl.elements].filter(item => {
+
+            if(item.name === "photo"){
+                return item;
+            }
+
+        });
+
+        let file = filePhoto[0].files[0];
+
+        fileReader.onload = () => {
+        
+            callBack(fileReader.result);
+
+        };
+
+        fileReader.readAsDataURL(file);
+    }
+
+    _getValues(){
+        
+        let user = {};
+
+        [...this.formEl.elements].forEach((field, index) => {
+            if(field.name == 'gender'){
+                if(field.checked){
+                    user[field.name] = field.value;
+                }
+            }else{
+                if(field.name == 'photo'){
+                    user[field.name] = field.value;
+                }else{
+                    user[field.name] = field.value;
+                }
+            }
+        });
+    
+        return new User(
+            user.name, 
+            user.gender, 
+            user.birth, 
+            user.country, 
+            user.email, 
+            user.password, 
+            user.photo, 
+            user.admin
+        );
+
+    }
+
+    _addLine(dataUser){
+        let tr = document.createElement("tr");
+        tr.innerHTML = `
+            <td>
+                <img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm">
+            </td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin}</td>
+            <td>${dataUser.data}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        `;
+        this.tableEl.appendChild(tr);
+    
+        /*
+            <tr>
+                <td>
+                    <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm">
+                </td>
+                <td>Fulano</td>
+                <td>fulano@hcode.com.br</td>
+                <td>Sim</td>
+                <td>02/04/2018</td>
+                <td>
+                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                </td>
+            </tr> 
+         */
+    }
+}
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let userController = new UserController("form-user-create", "table-users");
+
 /*
     function getUserInfo(){
         let $ = document.querySelector.bind(document);
@@ -21,70 +23,3 @@
         );   
     }
 */
-
-function addLine(dataUser){
-    let tr = document.createElement("tr");
-    tr.innerHTML = `
-        <td>
-            <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm">
-        </td>
-        <td>${dataUser.name}</td>
-        <td>${dataUser.email}</td>
-        <td>${dataUser.admin}</td>
-        <td>${dataUser.data}</td>
-        <td>
-            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-        </td>
-    `;
-    document.getElementById("table-users").appendChild(tr);
-
-    /*
-        <tr>
-            <td>
-                <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm">
-            </td>
-            <td>Fulano</td>
-            <td>fulano@hcode.com.br</td>
-            <td>Sim</td>
-            <td>02/04/2018</td>
-            <td>
-                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-            </td>
-        </tr> 
-     */
-}
-
-document.getElementById("form-user-create").addEventListener(("submit"), function(event){
-    event.preventDefault();
-    let form = document.querySelectorAll("#form-user-create [name]");
-    let user = {};
-
-    form.forEach((field, index) => {
-        if(field.name == 'gender'){
-            if(field.checked){
-                user[field.name] = field.value;
-            }
-        }else{
-            if(field.name == 'photo'){
-                user[field.name] = field.value;
-            }else{
-                user[field.name] = field.value;
-            }
-        }
-    });
-
-    var objectUser = new User(
-        user.name, 
-        user.gender, 
-        user.birth, 
-        user.country, 
-        user.email, 
-        user.password, 
-        user.photo, 
-        user.admin
-    );
-
-    addLine(objectUser);
-});
