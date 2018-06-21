838344b8b116cdf6a1b61a5d45e6c5d6a273ca16
Matheus de Araujo Delatorre <matheus.a.delatorre@gmail.com>
2018-06-21 12:04:31 -0300

    Utilização de DataSet e entendendo a conversão do obj em string com o JSON.stringify

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -12,42 +12,36 @@ class UserController{
             let botaoSubmit = document.querySelector("[type=submit]");
             botaoSubmit.disabled = true;
             let values = this._getValues();
-
-            this._getPhoto().then(
-                (content) => {
-                    values.photo = content;
-                    this._addLine(values); // Adiciona linha ao html
-                    this.formEl.reset();
-                    botaoSubmit.disabled = false;
-                },
-                (e) => {
-                    console.error(e);
-                }
-            );
-            
+            if(!values){
+                botaoSubmit.disabled = false;
+            }else{
+                this._getPhoto().then(
+                    (content) => {
+                        values.photo = content;
+                        this._addLine(values); // Adiciona linha ao html
+                        this.formEl.reset();
+                        botaoSubmit.disabled = false;
+                    },
+                    (e) => {
+                        console.error(e);
+                    }
+                );
+            }
         });
     }
 
     _getPhoto(callBack){
         return new Promise((resolve, reject) => {
             let fileReader = new FileReader();
-
             let filePhoto = [...this.formEl.elements].filter(item => {
-    
                 if(item.name === "photo"){
                     return item;
                 }
-    
             });
-    
             let file = filePhoto[0].files[0];
-    
             fileReader.onload = () => {
-            
                 resolve(fileReader.result);
-    
             };
-    
             fileReader.onerror = (e) => {
                 reject(e);
             };
@@ -60,16 +54,25 @@ class UserController{
     }
 
     _getValues(){
-        
         let user = {};
-
+        let isValid = true;
         [...this.formEl.elements].forEach((field, index) => {
+
+            if(['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value){
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+            }else{
+                if(['has-error'].indexOf(field.parentElement.classList)){
+                    field.parentElement.classList.remove('has-error');
+                }
+            }
+
             if(field.name == 'gender'){
                 if(field.checked){
                     user[field.name] = field.value;
                 }
             }else if(field.name == "admin"){
-                user[field.name] = (field.checked) ? "SIM" : "NÃO";
+                user[field.name] = (field.checked) ? true : false;
             }else{
                 if(field.name == 'photo'){
                     user[field.name] = field.value;
@@ -78,6 +81,8 @@ class UserController{
                 }
             }
         });
+
+        if(!isValid) return false;
     
         return new User(
             user.name, 
@@ -97,13 +102,16 @@ class UserController{
 
     _addLine(dataUser){
         let tr = document.createElement("tr");
+        tr.dataset.user = JSON.stringify(dataUser);
+        //Criando o dataset, o JSON não armazena os metodos. Então é necessário acessar os atridutos diretamente.
+        
         tr.innerHTML = `
             <td>
                 <img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm">
             </td>
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
-            <td>${dataUser.admin}</td>
+            <td>${(dataUser.admin) ? 'SIM' : 'NÃO'}</td>
             <td>${Helpers.dateFormatBr(dataUser.register)}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
@@ -111,21 +119,18 @@ class UserController{
             </td>
         `;
         this.tableEl.appendChild(tr);
-    
-        /*
-            <tr>
-                <td>
-                    <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm">
-                </td>
-                <td>Fulano</td>
-                <td>fulano@hcode.com.br</td>
-                <td>Sim</td>
-                <td>02/04/2018</td>
-                <td>
-                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                </td>
-            </tr> 
-         */
+        this._updateCount();
+    }
+
+    _updateCount(){
+        let numUsers    = 0;
+        let numAdmins   = 0;
+        [...this.tableEl.children].forEach(tr => {
+            numUsers++;
+            let user = JSON.parse(tr.dataset.user);
+            if(user._admin) numAdmins++;
+        });
+        document.querySelector('#number-users').innerHTML = numUsers;
+        document.querySelector('#number-admins').innerHTML = numAdmins;
     }
 }
